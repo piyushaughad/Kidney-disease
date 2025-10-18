c6c3c70a108a36560eb7b3eeee10febc4d0f2dd1
Piyush Aughad <piyushaughad@gmail.com>
2025-10-18 21:57:47 +0100

    added library and created env

---
+++ app.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# For handling imbalanced datasets by creating synthetic samples
+from imblearn.over_sampling import SMOTE
+
+# Statistical tools for diagnosing multicollinearity
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+from statsmodels.tools.tools import add_constant
+
+# For feature scaling
+from sklearn.preprocessing import StandardScaler
+
+# Machine learning models
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn import svm
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
+
+from sklearn import tree
+
+# Model selection and hyperparameter tuning
+from sklearn.model_selection import train_test_split, GridSearchCV
+
+# Evaluation metrics
+from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score,confusion_matrix
